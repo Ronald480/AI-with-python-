8046b76c6b5aeab6ac7387c88f3eed97bde613ba
RONALD ADU-MENSAH <150446900+Ronald480@users.noreply.github.com>
2024-05-15 03:27:52 +0000

    Add files via upload

---
+++ Trial.py
@@ -0,0 +1,9 @@
+# Step 1: Importing the required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn.linear_model import LinearRegression
+
+
+%matplotlib inline
